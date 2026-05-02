373270c4b6317cedb4fc62dab9542590a7f77ff2
Ahmad AL-Quraan <ahmadquraan.git@gmail.com>
2026-05-02 17:40:58 +0300

    First task

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,14 @@
+import sys
+
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    print(f"Program name: {sys.argv[0]}")
+    total_arg = len(sys.argv)
+    if total_arg == 1:
+        print("No arguments provided!")
+    else:
+        print(f"Arguments received: {total_arg -1}")
+        for i in range(len(sys.argv)):
+            if i != 0:
+                print(f"Argument {i}: {sys.argv[i]}")
+    print(f"Total arguments: {total_arg}")
